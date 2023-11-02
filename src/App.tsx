f23697a0a51ed00fd244f305045d87230fa15515
MinamizonoEno <133332898+MinamizonoEno@users.noreply.github.com>
2023-11-02 22:51:58 +0900

    Revert "Feature/front kaniery"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { CssBaseline } from '@mui/material';
 import { Routers } from './routes/Routers';
-import { Providers } from './provider/Providers';
 
 export const App = () => {
   return (
-    <Providers>
+    <>
       <CssBaseline />
       <Routers />
-    </Providers>
+    </>
   );
 };
